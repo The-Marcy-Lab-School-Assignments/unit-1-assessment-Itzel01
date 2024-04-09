156465186e2fc288d953921b2454cd35edbf7b39
Itzel01 <71834676+Itzel01@users.noreply.github.com>
2024-04-09 09:10:45 -0400

    Initial commit

---
+++ src/from-scratch.js
@@ -0,0 +1,63 @@
+const petJudger = () => {
+};
+
+const loopFromOneUpToAnother = () => {
+};
+
+const shoutEveryLetterForLoop = () => {
+};
+
+const shoutArrayMethod = () => {
+};
+
+const generateMultiplesOf5ToNum = () => {
+};
+
+const isLongArray = () => {
+};
+
+const addToFrontOrBack = () => {
+};
+
+const replaceMiddleValue = () => {
+};
+
+const getAllXCoordinates = () => {
+};
+
+const carMaker = () => {
+};
+
+const getFavoriteChildName = () => {
+};
+
+const getPessimisticTotal = () => {
+};
+
+const getNamesOfGreedyGnomes = () => {
+};
+
+const getAllNumbersFromString = () => {
+};
+
+// Do this only after all else
+const obliterate = () => {
+};
+
+module.exports = {
+  petJudger,
+  loopFromOneUpToAnother,
+  shoutEveryLetterForLoop,
+  shoutArrayMethod,
+  generateMultiplesOf5ToNum,
+  isLongArray,
+  addToFrontOrBack,
+  replaceMiddleValue,
+  getAllXCoordinates,
+  carMaker,
+  getFavoriteChildName,
+  getPessimisticTotal,
+  getNamesOfGreedyGnomes,
+  getAllNumbersFromString,
+  obliterate,
+};
---
+++ src/playground.js
@@ -0,0 +1,22 @@
+// const {
+//   petJudger,
+//   loopFromOneUpToAnother,
+//   shoutEveryLetterForLoop,
+//   shoutArrayMethod,
+//   generateMultiplesOf5ToNum,
+//   isLongArray,
+//   addToFrontOrBack,
+//   replaceMiddleValue,
+//   getAllXCoordinates,
+//   carMaker,
+//   getFavoriteChildName,
+//   getPessimisticTotal,
+//   getNamesOfGreedyGnomes,
+//   getAllNumbersFromString,
+//   obliterate,
+// } = require('./from-scratch');
+// const {
+//   fixVariables,
+//   doubleAllItemsPurely,
+//   addChildToParentMutation,
+// } = require('./debug');
